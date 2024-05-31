ecd060c74f88b744c3b08716018a91b5c7313738
EveGuo <zheng.eve.guo@gmail.com>
2024-05-31 00:08:28 -0500

    replace the map with my own map

--- src/constant.js
+++ src/constant.js
@@ -14,3 +14,18 @@ export const dialogueData = {
     library: `There are a lot of fiction books and some programming books on my shelves. I will read them in my spare time.`,
     exit: `If you want to exit Zheng's portfolio, just close the tab.`,
 };
+
+export const mydialogueData = {
+  mirror: `Hello, my name is Zheng Guo and I also go by Eve. I am currently residing in Texas, USA. I'm originally from China, but I decided to come to the United States for college in Chicago. After graduating, I stayed and started working here in the US.`,
+  pc: `This is my Desktop Computer. I work mostly in C# and TypeScript as a Full Stack Developer.
+    I also like Java and Python. Here is my <a href="https://github.com/eveguo1" target="_blank">Github</a>!`,
+  piano: `I like to listening to the music, and used to playing the piano when I was in high school. Taylor Swift is one of my favorite singers.`,
+  kitchen: `I will learn new dishes to cook occasionally. Recently I have learned how to make Brown Butter Cookies.`,
+  cert: `This is my Bachelor's of Science degree in Computer Science major. I graduated from Illinois Institute of Technology in 2019.`,
+  "sofa-table": `This is my sofa, I like to spend time here with my families.`,
+  tv: `That's my TV. I've been watching tech youtubers a lot.`,
+  resume: `This is my desk and on it is my resume. <a href="https://github.com/eveguo1/resume/blob/main/Zheng%20Guo%20Resume.pdf" target="_blank">Check it out!</a>
+  Contact me at zheng.eve.guo@gmail.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
+};
