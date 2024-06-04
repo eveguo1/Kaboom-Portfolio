1a6c94d909313fd4688b8a08d275c263f0bbd008
eveguo1 <zheng.eve.guo@gmail.com>
2024-06-04 00:59:10 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: "/Kaboom-Portfolio/",
+    base: "/",
     build: {
         // npm -D install terser
         minify: "terser"
